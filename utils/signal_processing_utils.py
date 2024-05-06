46d2e1f3b2b1b77e72d5dbabc23b91258b8fa8db
Yuqian Ma <yuqianm1@uci.edu>
2024-05-06 01:41:38 -0700

    fixed missing value

--- utils/signal_processing_utils.py
+++ utils/signal_processing_utils.py
@@ -46,14 +46,20 @@ def librosaextractFeatures(signal):
 
     ft = np.fft.fft(signal)
 
-    frequency_coefficients = np.mean(np.fft.fftfreq(len(ft)))
+    frequency_coefficients = np.mean(np.fft.fftfreq(len(ft))) #提取音频的频率
     fundamental_frequency, voiced_flag, voiced_probs = librosa.pyin(signal.astype('float'), fmin=librosa.note_to_hz('C2'),
-                                                 fmax=librosa.note_to_hz('C7'))
-    fundamental_frequency = np.nan_to_num(fundamental_frequency)
-    short_time_energy = np.mean(feature.rms(y=signal))
-    average_zero_cross_rate = np.mean(feature.zero_crossing_rate(y=signal.astype('float')))
+                                                 fmax=librosa.note_to_hz('C7')) #fundamental_frequency：基础音频
+
+    if np.isnan(fundamental_frequency).sum() == len(fundamental_frequency):
+        fundamental_frequency = np.nan_to_num(fundamental_frequency)
+    else:
+        fundamental_frequency = np.where(np.isnan(fundamental_frequency) == True, np.nanmean(fundamental_frequency), fundamental_frequency)
+
+    short_time_energy = np.mean(feature.rms(y=signal)) # 短时能量
+    average_zero_cross_rate = np.mean(feature.zero_crossing_rate(y=signal.astype('float'))) #过零率
     pitch_frequency = np.mean(69+12 * np.where(np.log2(fundamental_frequency/440) == -float('inf'), 0, np.log2(abs(fundamental_frequency)/440)))
-    fundamental_frequency = np.mean(fundamental_frequency)
+    # 音高，把音高为负无穷，即基础频率为0的地方的音高替换成0
+    fundamental_frequency = np.mean(fundamental_frequency) #取基础音频的平均值
 
     features = [frequency_coefficients, fundamental_frequency, short_time_energy
                 , average_zero_cross_rate, pitch_frequency]
